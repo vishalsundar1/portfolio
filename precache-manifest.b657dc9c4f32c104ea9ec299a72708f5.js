a91d4e914b16989fe91362f14a28699d0b4078bc
Vishal Sundareshwaran <vishal.sundar92@gmail.com>
2020-05-27 22:06:43 +0530

    Updates

---
+++ precache-manifest.b657dc9c4f32c104ea9ec299a72708f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a0c4046b8399b447878b6021905ec5e",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "c9b15636c9f24f4807b2",
+    "url": "/portfolio/static/css/main.b87af808.chunk.css"
+  },
+  {
+    "revision": "37cc640483fca1f24160",
+    "url": "/portfolio/static/js/2.5b3c6539.chunk.js"
+  },
+  {
+    "revision": "0f544458a968df1596e861027de46f92",
+    "url": "/portfolio/static/js/2.5b3c6539.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9b15636c9f24f4807b2",
+    "url": "/portfolio/static/js/main.8b543ed0.chunk.js"
+  },
+  {
+    "revision": "e5284505c87d0b24b753",
+    "url": "/portfolio/static/js/runtime-main.f4f2abbf.js"
+  }
+]);
